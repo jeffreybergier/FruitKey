ed51d516345d8a7635a8e3a0b8afc47d1b498885
jeffreybergier <support@saturdayapps.com>
2014-12-21 14:07:45 -0800

    Changed to MIT License
    Changed to MIT License

--- FKKeyboard/KeyboardViewController.swift
+++ FKKeyboard/KeyboardViewController.swift
@@ -1,9 +1,30 @@
 //
-//  KeyboardViewController.swift
-//  FKKeyboard
+// KeyboardViewController.swift
+// FKKeyboard
 //
-//  Created by Jeffrey Bergier on 10/29/14.
-//  Copyright (c) 2014 Saturday Apps. All rights reserved.
+// Created by Jeffrey Bergier on 10/29/14.
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 Jeffrey Bergier
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
 import UIKit
@@ -38,16 +59,14 @@ class KeyboardViewController: UIInputViewController {
         
         //create the view and add it to the view controller
         let nibArray:NSArray = NSBundle.mainBundle().loadNibNamed("FKKeyboardView", owner: self, options: nil)
-        var arrayIndex = 0
         for i in 0..<nibArray.count {
-            let keyboardView = nibArray[i] as FKKeyboardView
-            println(keyboardView.frame.size.height)
-            if keyboardView.frame.size.height == self.keyboardHeight {
-                arrayIndex = i
-                break
+            if let keyboardView = nibArray[i] as? FKKeyboardView {
+                if keyboardView.frame.size.height == self.keyboardHeight {
+                    self.keyboardView = keyboardView
+                    break
+                }
             }
         }
-        self.keyboardView = nibArray[arrayIndex] as? FKKeyboardView
         
         //check if this is an iPad
         if self.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.Regular && self.traitCollection.verticalSizeClass == UIUserInterfaceSizeClass.Regular {
